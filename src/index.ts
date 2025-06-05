bd0e6ef9948ebc468b20da5a2666fc780b6cdf2b
Pouya <pouya@kenshi.io>
2025-06-05 16:50:27 +0200

    Expose opcodes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Client } from "./client.js";
 export { Wallet } from "./wallet.js";
 export { Identity } from "./identity.js";
 export { Function } from "./function.js";
+export { OpCodes } from "./lib/opcodes.js";
