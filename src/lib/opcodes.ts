0706f684383690dfe91d663af70c6622ba95cef3
Pouya <pouya@kenshi.io>
2025-06-05 13:04:43 +0200

    Update opcodes

--- src/lib/opcodes.ts
+++ src/lib/opcodes.ts
@@ -1,12 +1,17 @@
 export const OpCodes = {
   Feedback: 0,
   Error: 1,
+
   Subscribe: 2,
   Unsubscribe: 3,
   Message: 4,
   Broadcast: 5,
+
   RegisterWorker: 6,
   WorkerOverload: 7,
-  RPCRequest: 8,
-  RPCResponse: 9,
+  TaskFailed: 8,
+
+  RPCRequest: 9,
+  RPCResponse: 10,
+  RPCStream: 11,
 };
